e31dd67a9fd6fd70cf60ce969a836e0a0eb63ff8
Arpit Rawat <44170759+arpitmailgun@users.noreply.github.com>
2021-02-09 17:32:56 -0600

    first commit with counter example

---
+++ pandas_tricks.py
@@ -0,0 +1,16 @@
+### Count column values. For example column with different technologies
+
+import collections
+import itertools
+
+data = pd.read_csv("data/sample_data.csv")
+data.columns = map(str.lower, data.columns)
+
+counter = collections.Counter(itertools.chain.from_iterable(v.split(',') for v in sample_data.techs))
+
+##Now keys from counter object will be tech and values will be the frequency of the tech in the techs column
+
+counter_list =[list(counter.keys()),list(counter.values())]
+
+#save the final result in a file
+pd.DataFrame(np.array(counter_list).T, columns=['Technology','Count']).sort_values(by='Count',ascending=False).to_csv('tech_list.csv',index=False)
